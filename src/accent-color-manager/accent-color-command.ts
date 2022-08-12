8841621797fd0692b3913acd4c2d9a32537eaf5a
Bijoya Brakenhoff <bbrakenhoff@gmail.com>
2022-08-12 10:53:57 +0200

    Quick pick an accent color

--- src/accent-color-manager/accent-color-command.ts
+++ src/accent-color-manager/accent-color-command.ts
@@ -1,6 +1,6 @@
 import * as vscode from 'vscode';
 import { SolarizedCustomThemeConfig } from './solarized-custom-theme-config';
-
+import { SolarizedColor } from '../solarized-color';
 export class AccentColorCommand {
   private static readonly IDENTIFIER = 'solarizedCustomTheme.setAccentColor';
 
@@ -13,17 +13,30 @@ export class AccentColorCommand {
   static register(): vscode.Disposable {
     return vscode.commands.registerCommand(
       this.IDENTIFIER,
-      AccentColorCommand.onSetAccentColorCommandTriggered
+      AccentColorCommand.onCommandSetAccentColor
     );
   }
 
-  private static onSetAccentColorCommandTriggered() {
+  private static async onCommandSetAccentColor() {
     const accentColorCommand = new AccentColorCommand();
     accentColorCommand.readSolarizedCustomThemeConfig();
 
+    const chosenAccentColor = await accentColorCommand.quickPick();
+
     vscode.window.showInformationMessage(
-      'onSetAccentColorCommandTriggered ' +
-        JSON.stringify(accentColorCommand.config)
+      'onSetAccentColorCommandTriggered ' + chosenAccentColor
+    );
+  }
+
+  private async quickPick() {
+    return vscode.window.showQuickPick(
+      this.getSolarizedColorKeysWithoutBaseColors()
+    );
+  }
+
+  private getSolarizedColorKeysWithoutBaseColors() {
+    return Object.keys(SolarizedColor).filter((key: string) =>
+      key.includes('Base')
     );
   }
 
