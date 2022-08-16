d93ed50bd8771633e9986e37e2c485298f346ca3
Bijoya Brakenhoff <bbrakenhoff@gmail.com>
2022-08-16 19:38:08 +0200

    Do not register DebugLogChannel

--- src/extension-main.ts
+++ src/extension-main.ts
@@ -1,9 +1,7 @@
 import * as vscode from 'vscode';
 import { AccentColorCommand } from './accent-color-command';
-import { DebugLogChannel } from './debug-log-channel';
 
 export async function activate(context: vscode.ExtensionContext) {
-  DebugLogChannel.create()
   // Register and subscribe solarizedCustomTheme.setAccentColor command
   context.subscriptions.push(
     vscode.commands.registerCommand(
